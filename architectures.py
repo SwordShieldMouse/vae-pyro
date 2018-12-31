2d04860aa6828906ecbe8cec83892a2f2a8452fc
SwordShieldMouse <alke8882000@gmail.com>
2018-12-31 00:41:17 -0500

    added main architectures

---
+++ architectures.py
@@ -0,0 +1,88 @@
+import torch
+import pyro
+import os
+import numpy as np
+import pyro.distributions as dist
+import torch.nn as nn
+
+pyro.enable_validation(True)
+
+class Decoder(nn.Module):
+    # the posterior distribution of x given z
+    # takes in a code z and outputs x
+    # why don't we output parameters for x, like with the encoder?
+    def __init__(self, z_dim, hidden_dim):
+        super(Decoder, self).__init__()
+
+        self.fc1 = nn.Linear(z_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 784) # 784 is the MNIST image size
+
+        self.leaky_relu = nn.LeakyReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, z):
+        hidden = self.softplus(self.fc1(z))
+        output = self.sigmoid(self.fc2(hidden)) # sigmoid since pixel values are in [0, 1]
+        return output
+
+def Encoder(nn.Module):
+    # the posterior dist of z given x
+    # takes in an input x and outputs parameters for a code z
+    def __init__(self, z_dim, hidden_dim):
+        super(Encoder, self).__init__()
+
+        self.fc1 = nn.Linear(784, hidden_dim)
+        self.fc21 = nn.Linear(hidden_dim, z_dim)
+        self.fc22 = nn.Linear(hidden_dim, z_dim)
+
+        self.leaky_relu = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = x.reshape(-1, 784)
+        hidden = self.leaky_relu(self.fc1(x))
+
+        z_loc = self.fc21(hidden)
+        z_scale = torch.exp(self.fc22(hidden)) # use exp since scale is positive
+
+        return z_loc, z_scale
+
+
+def VAE(nn.Module):
+    def __init__(self, z_dim = 50, hidden_dim = 400):
+        self.encoder = Encoder(z_dim, hidden_dim)
+        self.decoder = Decoder(z_dim, hidden_dim)
+
+        self.z_dim = z_dim
+        self.hidden_dim = hidden_dim
+
+    # the stochastic model p(x|z)p(z)
+    # with data, generate codes, then generate images
+    def model(self, x):
+        pyro.module("decoder", self.decoder) # so that pyro knows about parameters in decoder
+        with pyro.plate("data", x.shape[0]):
+            # hyperparameters for prior p(z)
+            z_loc = x.new_zeros([x.shape[0], self.z_dim])
+            z_scale = x.new_zeros([x.shape[0], self.z_dim])
+            z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1)) # dependent within rows
+
+            # decode the latent z
+            decoded = self.decoder(z)
+
+            # score against real images
+            pyro.sample("obs", dist.Bernoulli(decoded).to_event(1), obs = x.reshape(-1, 784)) # dependent within rows
+
+    # the variational distribution q(z|x)
+    def guide(self., x):
+        pyro.module("encoder", self.encoder)
+        with pyro.plate("data", x.shape[0]):
+            z_loc, z_scale = self.encoder(x)
+            pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+
+    def reconstruct_img(self, x):
+        # encode image x
+        z_loc, z_scale = self.encoder(x)
+        # sample in latent space
+        z = dist.Normal(z_loc, z_scale).sample()
+        # decode the image (note we don't sample in image space)
+        loc_img = self.decoder(z)
+        return loc_img
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import torch
-import pyro
+import torchvision.transforms as transforms
+
+from pyro.infer import SVI, Trace_ELBO
+from architectures import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+pyro.set_rng_seed(0)
+
+# dataset
+train_set = torch.datasets.MNIST(root = './data', train = True, download = True, transform = transforms.ToTensor())
+test_set = torch.datasets.MNIST(root = './data', train = False, download = True, transform = transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_set, batch_size = 10, shuffle = True)
+test_loader = torch.utils.data.DataLoader(dataset = test_set, batch_size = 10, shuffle = True)
+
+
+model = VAE()
+optim = pyro.optim.Adam({"lr": 1e-3})
+svi = SVI(vae.model, vae.guide, optim, loss = Trace_ELBO())
+
+# train
+print("Starting training")
